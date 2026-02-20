4146a5853039d24498d5dee94932908537da6bec
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-02-20 23:00:12 +0600

    added assts and set up files

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import Navbar from '../pages/Shared/Navbar/Navbar';
+import Footer from '../pages/Shared/Footer/Footer';
+
+const RootLayout = () => {
+    return (
+        <div className='flex flex-col min-h-screen'>
+            <Navbar />
+            <div className='w-11/12 lg:max-w-7xl mx-auto grow' >
+                <Outlet />
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default RootLayout;
---
+++ src/pages/Shared/ProFastLoogo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../assets/logo.png'
+
+const ProFastLoogo = () => {
+    return (
+        <div>
+            <img src={logo} alt="company logo" />
+            <p>ProFast</p>
+        </div>
+    );
+};
+
+export default ProFastLoogo;
